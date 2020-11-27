96dbbb7bb19a1ca9e3daf62671a42b5f82d261cf
museong Kang(XPS) <mskang1290@naver.com>
2020-11-27 10:43:44 +0900

    エラー修正

--- src/const/const.tsx
+++ src/const/const.tsx
@@ -1,5 +1,5 @@
 const Const = {
-  localhost: "https://development.d8zccqq3gmtke.amplifyapp.com/",
+  localhost: "https://development.d8zccqq3gmtke.amplifyapp.com",
   api: {
     getEvent: "/api/getEvent",
     addEvent: "/api/addEvent",
