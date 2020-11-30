4aa354365d568925f517018b42f30c5d3298a726
mskang1290 <active1492@gmail.com>
2020-11-30 18:30:47 +0900

    port number

--- src/const/const.tsx
+++ src/const/const.tsx
@@ -1,6 +1,6 @@
 const Const = {
   // localhost: "172.31.0.0/16",
-  localhost: "http://localhost:5000",
+  localhost: "http://aotschedule.herokuapp.com",
   // localhost: "https://development.d8zccqq3gmtke.amplifyapp.com",
   api: {
     getEvent: "/api/getEvent",
